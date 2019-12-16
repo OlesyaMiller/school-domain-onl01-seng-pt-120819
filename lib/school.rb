100078968b31d6d6da8c28480b12e787de268651
Olesya Miller <olesyam.miller@gmail.com>
2019-12-16 16:44:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+require 'pry'
+
+class School
+  attr_reader :name, :roster
+  
+  def initialize(name)
+    @name = name 
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if @roster.keys.include?(grade)
+      @roster[grade] << name 
+    else
+      @roster[grade] = []
+      @roster[grade] << name 
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end 
+  
+  def sort 
+    
+    @roster.map do |key, value|
+      value.sort 
+    end
+  end
+end
+
+
+
+
+
+
